2670e1e22d901de4ab92f0093734bfce4d40483c
Dasxunya <79106085+Dasxunya@users.noreply.github.com>
2024-08-04 13:49:15 +0300

    calculate rating

--- src/search_all_before_me.go
+++ src/search_all_before_me.go
@@ -6,50 +6,79 @@ import (
 	"github.com/antchfx/htmlquery"
 	"github.com/gocolly/colly"
 	"log"
+	"strconv"
 )
 
 func main() {
 	var identityNumber string
+	var url string
 
-	_, err := fmt.Scan(&identityNumber)
-	if err != nil {
-		log.Fatal(err)
+	fmt.Print("Введите ссылку на рейтинговый список (пример: https://abit.itmo.ru/rating/master/budget/1905): ")
+	_, errUrl := fmt.Scan(&url)
+	if errUrl != nil {
+		log.Fatal(errUrl)
 	}
 
-	fmt.Printf("Порядковый номер: %s", searchByIdentity(identityNumber))
+	fmt.Print("Введите СНИЛС: ")
+	_, errSnils := fmt.Scan(&identityNumber)
+	if errSnils != nil {
+		log.Fatal(errSnils)
+	}
+
+	var p, r = searchByIdentity(url, identityNumber)
+	fmt.Printf("\nВаш порядковый номер: %s\nВаш рейтинговый номер: %d", p, r)
 }
 
-func searchByIdentity(identityNumber string) string {
+func searchByIdentity(url string, identityNumber string) (string, int) {
 	var d string
+	var rating = 0
 
-	xPath := "//*[contains(@class, 'RatingPage_table__position')]"
-	url := "https://abit.itmo.ru/rating/master/budget/1905"
+	positionXPath := "//*[contains(@class, 'RatingPage_table__position')]"
+	itemXPath := "//*[contains(@class, 'RatingPage_table__item')]"
 	c := colly.NewCollector()
 
 	c.OnHTML("body", func(e *colly.HTMLElement) {
 		r := bytes.NewReader(e.Response.Body)
 		doc, err := htmlquery.Parse(r)
-		if err != nil {
-			log.Fatal(err)
-		}
+		throwFatal(err)
 
-		nodes, err := htmlquery.QueryAll(doc, xPath)
-		if err != nil {
-			log.Fatal(err)
-		}
+		positionNodes, err := htmlquery.QueryAll(doc, positionXPath)
+		throwFatal(err)
 
-		for _, node := range nodes {
+		itemNodes, err := htmlquery.QueryAll(doc, itemXPath)
+		throwFatal(err)
+
+		for _, node := range positionNodes {
 			if spanData := node.LastChild.FirstChild.Data; spanData == identityNumber {
 				d = node.FirstChild.Data
-				return
+				break
+			}
+		}
+
+		for _, node := range itemNodes {
+			pData := node.FirstChild.FirstChild.FirstChild.FirstChild.Data
+			isHasOriginal := node.LastChild.LastChild.LastChild.LastChild.LastChild.LastChild.Data
+			isFirstPriority := node.FirstChild.NextSibling.FirstChild.FirstChild.FirstChild.LastChild.FirstChild.Data
+			intPData, err1 := strconv.Atoi(pData)
+			throwFatal(err1)
+
+			intD, err2 := strconv.Atoi(d)
+			throwFatal(err2)
+
+			if intPData <= intD && isHasOriginal == "да" && isFirstPriority == "1" {
+				rating++
 			}
 		}
 	})
 
 	// Start scraping
 	err := c.Visit(url)
+	throwFatal(err)
+	return d, rating
+}
+
+func throwFatal(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	return d
 }
